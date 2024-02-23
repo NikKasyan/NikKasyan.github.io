20ec6e6c40ad51ba4a33c6f501e87abb6363f0dc
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-02-23 18:20:13 +0100

    Improve mobile version

--- htmz/main.js
+++ htmz/main.js
@@ -1,24 +1,29 @@
 
 
 const init = () => {
+    const burgerMenu = document.querySelector(".burger-menu");
+    const nav = document.querySelector("nav");
     (function injectNavLinks(){
         const links = document.querySelectorAll("nav>ul>li>a");
+
         links.forEach(link => {
             link.setAttribute("target", "htmz");
             link.addEventListener("click", function(){
+                burgerMenu.classList.remove("open");
+                nav.classList.remove("open");
                 removeActiveClass(links);
                 link.classList.add("active");
             });
         });
     })();
     (function injectBurgerMenu(){
-        const burgerMenu = document.querySelector(".burger-menu");
-        const nav = document.querySelector("nav");
         burgerMenu.addEventListener("click", function(){
             nav.classList.toggle("open");
             burgerMenu.classList.toggle("open");
         });
     })();
+
+
 
 
 }
