0903de0c96b45ba58180e90a16b212920c9d2b11
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-02-23 17:19:46 +0100

    Add burger menu

--- htmz/main.js
+++ htmz/main.js
@@ -11,6 +11,14 @@ const init = () => {
             });
         });
     })();
+    (function injectBurgerMenu(){
+        const burgerMenu = document.querySelector(".burger-menu");
+        const nav = document.querySelector("nav");
+        burgerMenu.addEventListener("click", function(){
+            nav.classList.toggle("open");
+            burgerMenu.classList.toggle("open");
+        });
+    })();
 
 
 }
